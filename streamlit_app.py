12bb20fb310172115774e72900fce8e3e645c995
molty-s <snow.avavel1216@gmail.com>
2026-05-03 11:24:48 +0900

    Update dataframe selection and display in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,9 @@ st.write('The current movie title is', title)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 options = st.multiselect(
     "Choose up to 5 ingredients",
