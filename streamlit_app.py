fca4493bc5fd23dc53adaf7262549300e88d14eb
molty-s <snow.avavel1216@gmail.com>
2026-05-03 10:54:04 +0900

    Fix case sensitivity in Snowflake connection string

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.write(
 title = st.text_input('Name on Smoothie:')
 st.write('The current movie title is', title)
 
-cnx = st.connection("Snowflake")
+cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
