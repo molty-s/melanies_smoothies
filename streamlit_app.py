325105c8020cf11b24f1bef46d237fd38d96fc6e
molty-s <snow.avavel1216@gmail.com>
2026-05-03 11:02:48 +0900

    Update smoothie response handling in streamlit_app.py
    Commented out the display of the smoothie response and added a dataframe to show the response data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,5 +40,6 @@ if options :
         st.success('Your Smoothie is ordered!', icon="✅")
 
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+# st.text(smoothiefroot_response)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
